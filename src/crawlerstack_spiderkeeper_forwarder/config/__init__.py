11a33c3a80fafd0001fb64daaafe5d99ad8bd637
qiang1218 <18792451160@163.com>
2023-02-03 14:02:51 +0800

    fix: 自定义配置日志获取参数化

--- src/crawlerstack_spiderkeeper_forwarder/config/__init__.py
+++ src/crawlerstack_spiderkeeper_forwarder/config/__init__.py
@@ -3,6 +3,7 @@ config
 """
 
 import os
+import sys
 from pathlib import Path
 
 from dynaconf import Dynaconf
@@ -16,11 +17,15 @@ settings_files = [
     Path(__file__).parent / 'settings.yml',
 ]  # 指定绝对路径加载默认配置
 
+_external_files = [
+    Path(sys.prefix, 'etc', 'SPIDERKEEPER', 'forwarder', 'settings.yml')
+]
+
 settings = Dynaconf(
     envvar_prefix="SPIDERKEEPER_FORWARDER",  # 环境变量前缀。设置`SPIDERKEEPER_FOO='bar'`，使用`settings.FOO`
     settings_files=settings_files,
     lowercase_read=False,  # 禁用小写访问， settings.name 是不允许的
-    includes=['/etc/spiderkeeper_forwarder/settings.yml'],  # 自定义配置覆盖默认配置
+    includes=_external_files,  # 自定义配置覆盖默认配置
     base_dir=base_dir,  # 编码传入配置
     localpath=local_path
 )
